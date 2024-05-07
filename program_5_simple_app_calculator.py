6ccb1ada5ad4bd11cd0231378db5eda4da4d3eb4
Kimberly <ae.xerx@gmail.com>
2024-05-08 00:24:57 +0800

    The'1. Addition will be displayed in light cyan

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -1,3 +1,4 @@
 #  Display the welcome message and options
 print("\033[38;5;208mSimple Calculator App \n\033[0m")
 print("Choose one of the four math operation:")
+print("\033[96m 1. Addition \033[0m")
