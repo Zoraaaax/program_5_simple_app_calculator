da922ff4a01d133e7e524ff742e0e27f1101bbbe
Kimberly <ae.xerx@gmail.com>
2024-05-08 02:26:02 +0800

    check_the_value_of_the_variable

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -49,3 +49,4 @@ while True:
             elif operation == 3:
                 result = first_number * second_number
                 print(f"\033[93mThe result of multiplication is {int(result) if result.is_integer() else result}\033[0m")
+            elif operation == 4:
