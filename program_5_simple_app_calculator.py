932f00e875c314b618e18aef877ed121ea174c01
Kimberly <ae.xerx@gmail.com>
2024-05-08 01:17:43 +0800

    fourth_math_operation

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -4,7 +4,7 @@ print("Choose one of the four math operations")
 print("\033[96m1. Addition\033[0m")
 print("\033[92m2. Subtraction\033[0m")
 print("\033[93m3. Multiplication\033[0m")
-print("\033[95m4. Division\033[0m")
+print("\033[95m4. Division\n\033[0m")
 
 #  Loop for the calculator functionality
 while True:
