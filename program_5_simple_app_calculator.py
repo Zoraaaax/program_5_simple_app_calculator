0f52e75009d9d87d3120c32ed5254b8f7c603bcf
Kimberly <ae.xerx@gmail.com>
2024-05-08 02:22:45 +0800

    calculates_the _product

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -47,4 +47,4 @@ while True:
                 result = first_number - second_number
                 print(f"\033[92mThe result of subtraction is{int(result) if result.is_integer() else result}\033[0m")
             elif operation == 3:
-                result = first_number * second_numbe
+                result = first_number * second_number
