177b2db3da89b8ab641fac36b3917eecbc8b9cb3
Kimberly <ae.xerx@gmail.com>
2024-05-08 02:35:14 +0800

    error_display_message

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -55,3 +55,4 @@ while True:
         except ZeroDivisionError:
             print("\033[91mZeroDivisionError: Division by zero is not allowed.\033[0m")
         except Exception as exception:
+            print("\033[91mAn unexpected error occurred:\033[0m", exception)
