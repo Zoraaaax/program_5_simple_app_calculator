054220ab75fcc3d2a339a0c1c1c094bfa08cd30a
Kimberly <ae.xerx@gmail.com>
2024-05-08 19:53:04 +0800

    message_display

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -71,3 +71,4 @@ while True:
                 print("\033[91mInvalid input! Please enter 'y' for yes and 'n' for no.\033[0m")
 
         if try_again != y:
+            print("Thank you! The program will exit.")
