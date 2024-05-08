263a48dec18bca7e525caaa256e84bdb35b7a035
Kimberly <ae.xerx@gmail.com>
2024-05-08 20:31:57 +0800

    check_the_value_of_the_variable

--- program_5_simple_app_calculator.py
+++ program_5_simple_app_calculator.py
@@ -49,7 +49,7 @@ while True:
         elif operation == 3:
             result = first_number * second_number
             print(f"\033[93mThe result of multiplication is {int(result) if result.is_integer() else result}\033[0m")
-            elif operation == 4:
+        elif operation == 4:
                 result = first_number / second_number
                 print(f"\033[95mThe result of division is {int(result) if result.is_integer() else result}\033[0m")
         except ZeroDivisionError:
